6a06e29ebb63937141f0ca84d9e8a853121c0ec5
wfica <wojtekfica@gmail.com>
2021-11-18 21:11:15 +0100

    Runtime 100%.

--- ex3/runtime.cpp
+++ ex3/runtime.cpp
@@ -111,17 +111,20 @@ void DebugPrintDist(const Board &board, const AllDirection dir) {
  **/
 
 AllDirection GetDirection(int x, int y, int prev_x, int prev_y) {
-  x -= prev_x;
-  y -= prev_y;
+  int dir_x = sgn(x - prev_x);
+  int dir_y = sgn(y - prev_y);
   for (int i = 0; i < 8; ++i) {
-    if (x == ALL_X[i] && y == ALL_Y[i]) {
+    if (dir_x == ALL_X[i] && dir_y == ALL_Y[i]) {
       return static_cast<AllDirection>(i);
     }
   }
-  throw logic_error("Could not find any matching direction.");
+  string error = to_string(prev_x) + "," + to_string(prev_y) + " --> " +
+                 to_string(x) + "," + to_string(y);
+  throw logic_error("Could not find any matching direction. " + error);
 }
 
 inline bool Cardinal(AllDirection dir) { return dir % 2 == 0; }
+inline bool Diagonal(AllDirection dir) { return dir % 2 == 1; }
 
 vector<AllDirection> ValidNextSteps(int x, int y, int prev_x, int prev_y) {
   if (prev_x == -1) return {N, NE, E, SE, S, SW, W, NW};
@@ -139,6 +142,32 @@ vector<AllDirection> ValidNextSteps(int x, int y, int prev_x, int prev_y) {
   return res;
 }
 
+bool ExactDirection(int x, int y, AllDirection dir, int goal_x, int goal_y) {
+  return (x == goal_x && sgn(ALL_Y[dir]) == sgn(goal_y - y)) ||
+         (y == goal_y && sgn(ALL_X[dir]) == sgn(goal_x - x));
+}
+
+bool GeneralDirection(int x, int y, AllDirection dir, int goal_x, int goal_y) {
+  return sgn(ALL_X[dir]) == sgn(goal_x - x) &&
+         sgn(ALL_Y[dir]) == sgn(goal_y - y);
+}
+
+inline int ManhatanDist(int x1, int y1, int x2, int y2) {
+  return abs(x1 - x2) + abs(y1 - y2);
+}
+
+// http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
+inline double HeuristicGeneral(int x1, int y1, int x2, int y2, double d,
+                               double d2) {
+  double dx = static_cast<double>(abs(x1 - x2));
+  double dy = static_cast<double>(abs(y1 - y2));
+  return d * (dx + dy) + (d2 - 2 * d) * min(dx, dy);
+}
+
+inline double HeuristicCost(int x1, int y1, int x2, int y2) {
+  return HeuristicGeneral(x1, y1, x2, y2, 1.0, M_SQRT2);
+}
+
 int main() {
   Board board;
   board.ReadBoard();
@@ -147,21 +176,23 @@ int main() {
 
   // In order of nodes visited by the JPS+ algorithm, a line containing
   // "nodeColumn nodeRow parentColumn parentRow givenCost".
-  cout << board.start_y << " " << board.start_x << " -1 -1 0.00" << endl;
+  // cout << board.start_y << " " << board.start_x << " -1 -1 0.00" << endl;
 
-  // -dist, x, y
-  priority_queue<tuple<float, int, int>> Q;
+  // -heuristic_dist, x, y
+  priority_queue<tuple<double, int, int>> Q;
   Q.push({0., board.start_x, board.start_y});
 
   pair<int, int> parent[MAX_SIZE][MAX_SIZE];
+  bool closed[MAX_SIZE][MAX_SIZE];
 
   for (int i = 0; i < MAX_SIZE; ++i) {
     for (int j = 0; j < MAX_SIZE; ++j) {
       parent[i][j] = {-1, -1};
+      closed[i][j] = false;
     }
   }
 
-  float distance[MAX_SIZE][MAX_SIZE];
+  double distance[MAX_SIZE][MAX_SIZE];
   for (int i = 0; i < MAX_SIZE; ++i) {
     for (int j = 0; j < MAX_SIZE; ++j) {
       distance[i][j] = MAX_SIZE * MAX_SIZE * MAX_SIZE;
@@ -170,25 +201,81 @@ int main() {
   distance[board.start_x][board.start_y] = 0.;
 
   // game loop
+  bool found = false;
   while (!Q.empty()) {
     // Write an action using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
     // In order of nodes visited by the JPS+ algorithm, a line containing
     // "nodeColumn nodeRow parentColumn parentRow givenCost".
 
-    float cost = -1 * get<0>(Q.top());
+    // double heuristic_cost = -1 * get<0>(Q.top());
     int x = get<1>(Q.top());
     int y = get<2>(Q.top());
     Q.pop();
+    if(closed[x][y]){
+      continue;
+    }
+    closed[x][y] = true;
+
+    cout << y << " " << x << " " << parent[x][y].second << " "
+         << parent[x][y].first << " " << distance[x][y] << endl;
     if (x == board.goal_x && y == board.goal_y) {
-      return 0;
+      found = true;
+      break;
     }
     int prev_x = parent[x][y].first;
     int prev_y = parent[x][y].second;
 
-    for(const AllDirection dir : ValidNextSteps(x, y, prev_x, prev_y)){
-    }
+    for (const AllDirection dir : ValidNextSteps(x, y, prev_x, prev_y)) {
+      // cerr << "poss dir: " << dir << endl;
+      int next_x = -1, next_y = -1;
+      double cost;
+      // cerr << Cardinal(dir) << endl
+      //      << ExactDirection(x, y, dir, board.goal_x, board.goal_y) << endl
+      //      << ManhatanDist(x, y, board.goal_x, board.goal_y) << endl
+      //      << abs(board.dist_[x][y][dir]) << endl;
+      if (Cardinal(dir) &&
+          ExactDirection(x, y, dir, board.goal_x, board.goal_y) &&
+          ManhatanDist(x, y, board.goal_x, board.goal_y) <=
+              abs(board.dist_[x][y][dir])) {
+        // Goal is closer than wall distance or closer than or equal to jump
+        // point distance.
+        next_x = board.goal_x;
+        next_y = board.goal_y;
+        cost = distance[x][y] + ManhatanDist(x, y, board.goal_x, board.goal_y);
+      } else if (Diagonal(dir) &&
+                 GeneralDirection(x, y, dir, board.goal_x, board.goal_y) &&
+                 min(abs(x - board.goal_x), abs(y - board.goal_y)) <=
+                     abs(board.dist_[x][y][dir])) {
+        // Goal is closer or equal in either row or column than wall or jump
+        // point distance.
+        int jump_len = min(abs(x - board.goal_x), abs(y - board.goal_y));
+        next_x = x + ALL_X[dir] * jump_len;
+        next_y = y + ALL_Y[dir] * jump_len;
+        cost = distance[x][y] + M_SQRT2 * jump_len;
+      } else if (board.dist_[x][y][dir] > 0) {
+        // Jump point in this direction.
+        int jump_len = board.dist_[x][y][dir];
+        next_x = x + ALL_X[dir] * jump_len;
+        next_y = y + ALL_Y[dir] * jump_len;
+        cost = distance[x][y] + (Diagonal(dir) ? M_SQRT2 : 1.0) * jump_len;
+      }
 
-    cout << "3 4 0 2 3.14" << endl;
+      // Traditional A* from this point.
+      if (next_x != -1) {
+        // cerr << "pos next move: " << next_x << ", " << next_y << endl;
+        if (!closed[next_x][next_y] && cost < distance[next_x][next_y]) {
+          parent[next_x][next_y] = {x, y};
+          distance[next_x][next_y] = cost;
+          double heuristic_cost =
+              cost + HeuristicCost(next_x, next_y, board.goal_x, board.goal_y);
+          Q.push({-heuristic_cost, next_x, next_y});
+        }
+      }
+    }
+  }
+
+  if (!found) {
+    cout << "NO PATH" << endl;
   }
 }
